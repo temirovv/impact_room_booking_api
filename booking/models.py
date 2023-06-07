69021f0c5f6405ad74a624a658fa220ffc9585b9
temirovv <temirovv21@gmail.com>
2023-06-08 00:27:15 +0500

    models created

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Resident(models.Model):
+    name = models.CharField(max_length=150)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=150)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Booking(models.Model):
+    resident = models.ForeignKey(Resident, on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING, related_name="bookings")
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.room} booked by {self.resident}"
+
+
+class AvailableRooms(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING, related_name="availablity")
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    is_available = models.BooleanField(default=True)
+
+
