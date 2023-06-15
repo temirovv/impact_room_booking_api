572a0458d0ed7b8247f69759c61f7e5d9ca6060f
temirovv <temirovv21@gmail.com>
2023-06-15 17:39:17 +0500

    Created test for RoomDetailView

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Room
+from .serializers import RoomSerializer
+
+
+class RoomDetailTest(APITestCase):
+    def setUp(self):
+        self.room = Room.objects.create(name="Room test1", room_type='conference', capacity=5)
+
+    def test_existing_room(self):
+        url = reverse('room-detail', args=[self.room.pk])
+        response = self.client.get(url, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, RoomSerializer(self.room).data)
+
+    def test_nonexisting_room(self):
+        url = reverse('room-detail', args=[1000])
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data, {"error": "topilmadi"})
