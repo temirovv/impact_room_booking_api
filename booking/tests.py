472631271f9e14295aa3df9713de79e90c69ddd1
temirovv <temirovv21@gmail.com>
2023-06-15 22:30:28 +0500

    created a test for booking room

--- booking/tests.py
+++ booking/tests.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+from datetime import date
+
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.utils import timezone
 from django.urls import reverse
 
-from .models import Room
+from .models import Room, Booking
 from .serializers import RoomSerializer
 
 
 class RoomDetailTest(APITestCase):
     def setUp(self):
-        self.room = Room.objects.create(name="Room test1", room_type='conference', capacity=5)
+        self.room = Room.objects.create(name='Room test1', room_type='conference', capacity=5)
 
     def test_existing_room(self):
         url = reverse('room-detail', args=[self.room.pk])
@@ -20,6 +24,51 @@ class RoomDetailTest(APITestCase):
     def test_nonexisting_room(self):
         url = reverse('room-detail', args=[1000])
         response = self.client.get(url)
-        
+
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertEqual(response.data, {"error": "topilmadi"})
+        self.assertEqual(response.data, {'error': 'topilmadi'})
+
+class BookingRoom(APITestCase):
+    def setUp(self):
+        self.room = Room.objects.create(name='traning room', room_type='focus', capacity=9)
+        today = timezone.localdate()
+        
+        day, month, year = today.day, today.month, today.year
+        try:
+            booking_date = date(day=day+1, month=month, year=year).strftime('%d-%m-%Y')
+        except ValueError:
+            booking_date = date(year=year, month=month+1, day=1)
+        
+        booking_date = str(booking_date)
+        self.booking = {
+            'resident': {'name': 'Residentjon'},
+            'start': f'{booking_date} 09:00:00',
+            'end': f'{booking_date} 11:00:00'
+        }
+
+    def test_invalid_booking_data(self):
+        url = reverse('room-booking', args=[self.room.pk])
+        invalid_data = self.booking.copy()
+        invalid_data['resident']['name'] = ''
+
+        response = self.client.post(url, invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'error': 'Resident nomi bo‘sh bo‘lishi mumkin emas'})
+
+    def test_valid_and_invalid_booking(self):
+        url = reverse('room-booking', args=[self.room.pk])
+        # valid booking
+        response = self.client.post(url, data=self.booking, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'message': 'xona muvaffaqiyatli band qilindi'})    
+        
+        booking = Booking.objects.last()
+        self.assertEqual(booking.room, self.room)
+        self.assertEqual(booking.resident.name, self.booking['resident']['name'])
+        # invalid booking
+        response = self.client.post(url, data=self.booking, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_410_GONE)
+        self.assertEqual(response.data, {'error': 'uzr, siz tanlagan vaqtda xona band'})
+
