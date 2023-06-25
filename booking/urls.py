137a766eb4dca113294bab550ba09d2e42ce2eaf
temirovv <temirovv21@gmail.com>
2023-06-25 13:51:29 +0500

    added name field to the room availability

--- booking/urls.py
+++ booking/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', RoomListAPIView.as_view(), name='rooms'),
     path('<int:pk>/', RoomDetailView.as_view(), name='room-detail'),
     path("<int:pk>/book/", BookingRoomView.as_view(), name='room-booking'),
-    path("<int:pk>/availability/", RoomAvailabiltyAPIView.as_view()),
+    path("<int:pk>/availability/", RoomAvailabiltyAPIView.as_view(), name='availability'),
 ]
