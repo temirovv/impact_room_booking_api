ac9e9efbd21f9c757e094b3c4d731d97c0557d2d
temirovv <temirovv21@gmail.com>
2023-06-25 09:21:47 +0500

    gave access to the JPRQ server

--- room_booking/settings.py
+++ room_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n)%+c098#-%ik8=f2f%1h=txlq0r74(_#jl*+!frtici2-%$b2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['temirovv.jprq.live', '127.0.0.1']
 
 
 # Application definition
