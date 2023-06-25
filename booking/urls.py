f240e473c287020b399c3357ae1607309eb44bd6
temirovv <temirovv21@gmail.com>
2023-06-25 11:56:15 +0500

    added names to the views

--- booking/urls.py
+++ booking/urls.py
@@ -3,8 +3,8 @@ from .views import RoomListAPIView, RoomDetailView, BookingRoomView, RoomAvailab
 
 
 urlpatterns = [
-    path('', RoomListAPIView.as_view()),
-    path('<int:pk>/', RoomDetailView.as_view()),
-    path("<int:pk>/book/", BookingRoomView.as_view()),
+    path('', RoomListAPIView.as_view(), name='rooms'),
+    path('<int:pk>/', RoomDetailView.as_view(), name='room-detail'),
+    path("<int:pk>/book/", BookingRoomView.as_view(), name='room-booking'),
     path("<int:pk>/availability/", RoomAvailabiltyAPIView.as_view()),
 ]
